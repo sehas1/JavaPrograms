71897f9abb9c4539fbd87b36aaf7b67bc5c2a556
Tornado Test <tornadotest@Tornados-MacBook-Air.local>
2020-06-22 18:07:16 -0700

    adding 3 files

--- javaPrograms/src/javaPrograms/CountWords.java
+++ javaPrograms/src/javaPrograms/CountWords.java
@@ -1,27 +1,21 @@
 package javaPrograms;
 
 public class CountWords {
-	public static void main(String arg[])
-	{
-		String st="SSSsehas";
-		
-		int count[]=new int[256];
-	
-		
-		for(char x:st.toCharArray())
-		{
-			int ascii=x;
-			count[ascii]=++count[ascii];
+	public static void main(String arg[]) {
+		String st = "SSSsehas is a student";
+
+		int count[] = new int[256];
+
+		for (char x : st.toCharArray()) {
+			int ascii = x;
+			count[ascii] = ++count[ascii];
 		}
-		
-		for(int y:count)
-		{
-			if(y!=0)
-			System.out.print(y+"\n");
-			
-			
+
+		for (int j = 0; j < count.length; j++) {
+			if (count[j] != 0)
+				System.out.println("The character " + (char) j + " is repeated " + count[j]);
+
 		}
-		
 	}
 
 }
--- javaPrograms/src/javaPrograms/MyArrayList.java
+++ javaPrograms/src/javaPrograms/MyArrayList.java
@@ -2,69 +2,56 @@ package javaPrograms;
 
 import java.util.Arrays;
 
-public class MyArrayList < E > {
-Object bucket[];
- private int size;
-	MyArrayList()
-	{
-		size=0;
-		bucket=new Object[10];
-		
-	}
-	
-	
-public	void get(int index)
-	{
-	if(index<0 || index>size)
-		throw new ArithmeticException("Wrong index value");
-	else
-	{
-	    Object value=bucket[index];
-	    System.out.println("The value is "+value);
-	    
-	}
-	
+public class MyArrayList<E> {
+	Object bucket[];
+	private int size;
+
+	MyArrayList() {
+		size = 0;
+		bucket = new Object[10];
+
 	}
 
+	public void get(int index) {
+		if (index < 0 || index > size)
+			throw new ArithmeticException("Wrong index value");
+		else {
+			Object value = bucket[index];
+			System.out.println("The value is " + value);
 
+		}
 
-public void add(E addValue)
-{
- 
-	if(size>bucket.length-1)
-		increaseSize(bucket);
-	bucket[size++]=addValue;
-}
+	}
 
-private void increaseSize(Object[] bucket2) {
-	// TODO Auto-generated method stub
-	
-	 int newIncreasedCapacity = bucket.length * 2;
-	 bucket = Arrays.copyOf(bucket, newIncreasedCapacity);
-	
-	
-	
+	public void add(E addValue) {
 
-}
+		if (size > bucket.length - 1)
+			increaseSize(bucket);
+		bucket[size++] = addValue;
+	}
 
+	private void increaseSize(Object[] bucket2) {
+		// TODO Auto-generated method stub
 
-public void delete(int index)
-{
-	if(index<0 || index>size)
-		throw new ArithmeticException("Wrong index value");
- for(int i=index;i<bucket.length-1;i++)
- {
-	 bucket[i]=bucket[i+1];
- }
- System.out.println("After deletion");
- --size;
- this.display();
+		int newIncreasedCapacity = bucket.length * 2;
+		bucket = Arrays.copyOf(bucket, newIncreasedCapacity);
 
-}
+	}
 
-public void display()
-{
-	for(int j=0;j<size;j++)
-		System.out.println(bucket[j]+"\n");
-}
+	public void delete(int index) {
+		if (index < 0 || index > size)
+			throw new ArithmeticException("Wrong index value");
+		for (int i = index; i < bucket.length - 1; i++) {
+			bucket[i] = bucket[i + 1];
+		}
+		System.out.println("After deletion");
+		--size;
+		this.display();
+
+	}
+
+	public void display() {
+		for (int j = 0; j < size; j++)
+			System.out.println(bucket[j] + "\n");
+	}
 }
